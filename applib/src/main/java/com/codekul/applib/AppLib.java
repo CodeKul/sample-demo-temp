8834b2984bab7f5945a5ea60b67b2f6d5dfabd93
aniruddha <aniruddha.kudalkar@gmail.com>
2017-05-22 12:16:46 +0530

    first commit

---
+++ applib/src/main/java/com/codekul/applib/AppLib.java
@@ -0,0 +1,12 @@
+package com.codekul.applib;
+
+/**
+ * Created by aniruddha on 22/5/17.
+ */
+
+public class AppLib {
+
+    public void version() {
+
+    }
+}
---
+++ sample/src/main/java/com/codekul/sample/Sample.java
@@ -0,0 +1,12 @@
+package com.codekul.sample;
+
+import com.codekul.applib.AppLib;
+
+/**
+ * Created by aniruddha on 22/5/17.
+ */
+
+public class Sample {
+
+    private AppLib lib;
+}
