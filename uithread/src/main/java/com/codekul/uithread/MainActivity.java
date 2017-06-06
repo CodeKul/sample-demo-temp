5f5aee602ac94e91425135c509f4893c4a0e84da
aniruddha <aniruddha.kudalkar@gmail.com>
2017-06-06 12:55:14 +0530

    ui thread: handlers

---
+++ uithread/src/main/java/com/codekul/uithread/MainActivity.java
@@ -0,0 +1,54 @@
+package com.codekul.uithread;
+
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class MainActivity extends AppCompatActivity {
+
+    int i = 0;
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        handler = new Handler(Looper.getMainLooper());
+    }
+
+
+    public void onBtnClick(View view) {
+
+
+    }
+
+    private void handlerDemo() {
+
+    }
+
+    private void workerThread() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    // ((TextView) findViewById(R.id.txtCntr)).setText("" + i);
+                }
+            }
+        }).start();
+    }
+}
